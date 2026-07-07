43ef0039a3bed7f230e5f4316767f89facc21d48
El <dkorotkov14@gmail.com>
2026-07-07 20:15:14 +0300

    Add CSRF_TRUSTED_ORIGINS

--- Django/settings.py
+++ Django/settings.py
@@ -11,6 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://127.0.0.1:8000,http://localhost:8000').split(',')
 
 # Application definition
 
