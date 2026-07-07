3eeee638073f2c91657441a21861ffbb7af6c149
El <dkorotkov14@gmail.com>
2026-07-07 20:33:56 +0300

    5. Explained Why source_id

--- parser/tasks.py
+++ parser/tasks.py
@@ -20,11 +20,18 @@ def get_cached_keywords():
     )
 
 
-#
-# Парсинг одного конкретного источника
-#
 @shared_task(name="parser.tasks.celery_crawl_single_source")
 def celery_crawl_single_source(source_id: int):
+    """
+        Основная задача Celery для парсинга ОДНОГО источника.
+
+        Зачем здесь source_id:
+        1. Сбой для одного источника не ломает весь процесс для других.
+        2. Целери распределяет эти задачи по разным воркерам для одновременного запуска.
+        3. Целери сереализует аргументы в JSON для передачи в Редис, но Django-модели напрямую не сереализуются.
+        Даже если бы и сереализовались, то во-первых это было бы неэффективно, а во-вторых, данные могли бы устареть к моменту выполнения задачи.
+        Поэтому передается только (примитив) во избежание проблем с сериализацией Django-моделей и устареванием данных.
+    """
     try:
         source = Source.objects.get(pk=source_id, isActive=True)
 
@@ -36,23 +43,23 @@ def celery_crawl_single_source(source_id: int):
         logger.exception(f"Ошибка при парсинге источника {source_id}: {e}")
 
 
-#
-# Роутер для Админки
-# Задача для запуска парсинга нескольких источников по их ID.
-#
 @shared_task(name="parser.tasks.celery_crawl_sources_task")
 def celery_crawl_sources_task(source_ids):
+    """
+        Задача-роутер. Принимает список ID из админ-панели (Django bulk action)
+        и дробит их на множество независимых параллельных подзадач celery_crawl_single_source.
+    """
     s_ids = Source.objects.filter(pk__in=source_ids, isActive=True).values_list('id', flat=True)
     for s_id in s_ids:
         celery_crawl_single_source.delay(s_id)
 
 
-#
-# Роутер для Beat и Кнопки
-# Глобальная задача для запуска всех активных парсеров.
-#
 @shared_task(name="parser.tasks.celery_crawl_all_active_sources_task")
 def celery_crawl_all_active_sources_task():
+    """
+        Задача-роутер для запуска всех активных источников.
+        Берет все активные источники из базы и создает для каждого отдельную задачу celery_crawl_single_source.
+    """
     active_ids = Source.objects.filter(isActive=True).values_list('id', flat=True)
     for s_id in active_ids:
         celery_crawl_single_source.delay(s_id)
